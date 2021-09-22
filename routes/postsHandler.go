9a68c7ff944115129f89840251887198dde216c7
IllusionMan1212 <hisham.abourgheba@gmail.com>
2021-09-23 01:58:04 +0300

    fix: forgot to fix these queries

--- routes/postsHandler.go
+++ routes/postsHandler.go
@@ -207,9 +207,10 @@ func GetPosts(w http.ResponseWriter, req *http.Request) {
 			post.content as post_content, post.created_at as post_created_at,
 			parent.id as parent_id, parent.content as parent_content,
 			parent_author.username as parent_author_username, parent_author.display_name as parent_author_display_name, parent_author.avatar_url as parent_author_avatar_url,
-			ARRAY_AGG(DISTINCT attachments.url) as attachments_urls, ARRAY_AGG(attachments.type) as attachments_types,
+			(SELECT ARRAY_AGG(attachments.url) FROM attachments WHERE attachments.post_id = post.id) as attachments_urls,
+			(SELECT ARRAY_AGG(attachments.type) FROM attachments WHERE attachments.post_id = post.id) as attachments_types,
 			(SELECT count(likes) FROM likes WHERE likes.post_id = post.id) as likes,
-			count(comments) as comments,
+			count(DISTINCT comments) as comments,
 			EXISTS (SELECT user_id FROM likes WHERE likes.post_id = post.id AND likes.user_id = $3)
 				FROM posts post
 				LEFT JOIN posts parent
@@ -232,9 +233,10 @@ func GetPosts(w http.ResponseWriter, req *http.Request) {
 			post.content as post_content, post.created_at as post_created_at,
 			parent.id as parent_id, parent.content as parent_content,
 			parent_author.username as parent_author_username, parent_author.display_name as parent_author_display_name, parent_author.avatar_url as parent_author_avatar_url,
-			ARRAY_AGG(DISTINCT attachments.url) as attachments_urls, ARRAY_AGG(attachments.type) as attachments_types,
+			(SELECT ARRAY_AGG(attachments.url) FROM attachments WHERE attachments.post_id = post.id) as attachments_urls,
+			(SELECT ARRAY_AGG(attachments.type) FROM attachments WHERE attachments.post_id = post.id) as attachments_types,
 			(SELECT count(likes) FROM likes WHERE likes.post_id = post.id) as likes,
-			count(comments) as comments,
+			count(DISTINCT comments) as comments,
 			EXISTS (SELECT user_id FROM likes WHERE likes.post_id = post.id AND likes.user_id = $3) as liked
 				FROM posts post
 				LEFT JOIN posts parent
@@ -265,9 +267,10 @@ func GetPosts(w http.ResponseWriter, req *http.Request) {
 	post.content as post_content, post.created_at as post_created_at,
 	parent.id as parent_id, parent.content as parent_content,
 	parent_author.username as parent_author_username, parent_author.display_name as parent_author_display_name, parent_author.avatar_url as parent_author_avatar_url,
-	ARRAY_AGG(DISTINCT attachments.url) as attachments_urls, ARRAY_AGG(attachments.type) as attachment_types,
+	(SELECT ARRAY_AGG(attachments.url) FROM attachments WHERE attachments.post_id = post.id) as attachments_urls,
+	(SELECT ARRAY_AGG(attachments.type) FROM attachments WHERE attachments.post_id = post.id) as attachments_types,
 	(SELECT count(likes) FROM likes WHERE likes.post_id = post.id) as likes,
-	count(comments) as comments,
+	count(DISTINCT comments) as comments,
 	EXISTS (SELECT user_id FROM likes WHERE likes.post_id = post.id AND likes.user_id = $2) as liked
 		FROM posts post
 		LEFT JOIN posts parent
